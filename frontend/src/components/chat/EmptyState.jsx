30f3d680cb1ab6ecd0081a01ade109411dd9b584
Subhadeep-pan <subhadeeppan047@gmail.com>
2026-09-06 11:02:25 +0530

    Fix remaining merge conflict

--- frontend/src/components/chat/EmptyState.jsx
+++ frontend/src/components/chat/EmptyState.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 8ddc0bc (prepare backend for Render)
 export default function EmptyState() {
   return (
     <div className="flex flex-col items-center justify-center text-center mt-24">
